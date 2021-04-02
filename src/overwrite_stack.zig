dd49b724957a37c1238c0762ebbf87bfd1975477
Roman Frołow <rofrol@gmail.com>
2021-04-02 18:25:39 +0200

    overwrite_stack.zig: missing *

--- src/overwrite_stack.zig
+++ src/overwrite_stack.zig
@@ -1,13 +1,10 @@
 // https://zigforum.org/t/annoying-things/208/5
-// rofrol: How to reproduce it?
-// as(Sobeston, undefined): This isn't something that you can, from a language perspective, reliably reproduce
-// You don't define where and how locals are stored
 
 const std = @import("std");
 
 fn oopsie() *u32 {
     var x: u32 = 123;
-    std.debug.print("&x: {_}\n", .{&x});
+    std.debug.print("&x: {*}\n", .{&x});
     return &x;
 }
 
