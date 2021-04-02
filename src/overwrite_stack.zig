d6df06e5b46653de1320cf904ce3a4ad2fd00ff4
Roman Frołow <rofrol@gmail.com>
2021-04-02 18:00:43 +0200

    overwrite stack update

--- src/overwrite_stack.zig
+++ src/overwrite_stack.zig
@@ -1,4 +1,7 @@
 // https://zigforum.org/t/annoying-things/208/5
+// rofrol: How to reproduce it?
+// as(Sobeston, undefined): This isn't something that you can, from a language perspective, reliably reproduce
+// You don't define where and how locals are stored
 
 const std = @import("std");
 
@@ -6,6 +9,11 @@ fn oopsie() *u32 {
     var x: u32 = 123;
     std.debug.print("&x: {_}\n", .{&x});
     return &x;
+}
+
+fn f() *i32 {
+    var i: i32 = 44;
+    return &i;
 }
 
 fn dosomething() void {
