4fa44633f3a7478a88739ab78e6361c55156f5ca
Roman Frołow <rofrol@gmail.com>
2021-05-13 13:29:40 +0200

    src/struct_and_this.zig

--- src/struct_and_this.zig
+++ src/struct_and_this.zig
@@ -1,9 +1,11 @@
 const print = @import("std").debug.print;
 
 const Bar = struct {
+    const Self = @This();
     number: u32,
 
-    pub fn printMe(self: *Bar) void {
+    // pub fn printMe(self: *@This()) void { // Also works
+    pub fn printMe(self: *Self) void {
         print("{}\n", .{self.number});
     }
 };
