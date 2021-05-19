da2da2e8602a334654cb6241ef87e26aff333a29
Roman Frołow <rofrol@gmail.com>
2021-05-19 14:20:37 +0200

    src/struct_and_this.zig

--- src/struct_and_this.zig
+++ src/struct_and_this.zig
@@ -4,12 +4,15 @@ const Bar = struct {
     const Self = @This();
     number: u32,
 
+    // cast discards const qualifier
     // error: expected type '*Bar', found '*const Bar
     // pub fn printMe(self: *@This()) void {
     // pub fn printMe(self: *Self) void {
 
     // pub fn printMe(self: Self) void { // also works
-    pub fn printMe(self: @This()) void {
+    // pub fn printMe(self: @This()) void {
+    // https://github.com/ratfactor/ziglings/pull/54#issuecomment-841265975
+    pub fn printMe(self: *const @This()) void {
         print("{}\n", .{self.number});
     }
 };
@@ -17,4 +20,6 @@ const Bar = struct {
 pub fn main() !void {
     const bar = Bar{ .number = 1000 };
     bar.printMe();
+    var bar2 = Bar{ .number = 1001 };
+    bar2.printMe();
 }
